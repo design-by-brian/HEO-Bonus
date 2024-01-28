bda617e824c8a33cba1df22ff62a24e486354024
Brian Smith <19463540@student.curtin.edu.au>
2024-01-28 23:35:41 +1100

    added drop down for selecting space station added jan tle for css and ISS refactored orbit.py

--- app.py
+++ app.py
@@ -1,33 +1,50 @@
-from dash import Dash, dcc, html, Input, Output
-import plotly.graph_objects as go
+from dash import dcc, html, Input, Output
+import dash_bootstrap_components as dbc
 import plotly.express as px
 from orbit import get_orbit
+import pandas as pd
+import numpy as np
+import dash
 
-tle = []
-with open('tianhe-tle-short.txt',"r") as f:
-    tle = f.readlines()
+# Earth orbit parameters
+mu = 398600.4418
+r = 6500 #6781
+D = 24*0.997269
 
-app = Dash(__name__)
+uu, vv = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
+xx = r * np.cos(uu) * np.sin(vv)
+yy = r * np.sin(uu) * np.sin(vv)
+zz = r * np.cos(vv)
+
+tianhe_tle = []
+with open('tianhe-tle.txt',"r") as f:
+    tianhe_tle = f.readlines()
+
+ISS_tle = []
+with open('ISS-tle.txt', "r") as f:
+    ISS_tle = f.readlines()
+
+tle_df = pd.DataFrame({'Chinese Space Station - CSS': tianhe_tle, 'International Space Station - ISS': ISS_tle})
+
+app = dash.Dash(external_stylesheets=[dbc.themes.LUX])
 server = app.server
 
 app.layout = html.Div([
-    html.H4('Historical orbits of ISS and Tiangong space stations.'),
-    html.P("Select station:"),
-    dcc.Dropdown(
-        id="dropdown",
-        options=['ISS', 'Tiangong'],
-        value='ISS',
-        clearable=False,
-    ),
-    dcc.Graph(id="graph"),
+    html.H1(children='Historical Orbits for January', style={'textAlign':'center'}),
+    dcc.Dropdown(['Chinese Space Station - CSS', 'International Space Station - ISS'], 'Chinese Space Station - CSS', id='dropdown-selection'),
+    dcc.Graph(id='graph-content')
 ])
 
 @app.callback(
-    Output("graph", "figure"), 
-    Input("dropdown", "value"))
-def plot_tle(color):
-    orbit_data = get_orbit(tle)
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_graph(value):
+    orbit_data = get_orbit(tle_df[value])
     fig = px.line_3d(orbit_data, x="x", y="y", z="z", width=800, height=800)
+    fig.add_surface(x=xx, y=yy, z=zz, showscale=False)
+    fig.update(layout_coloraxis_showscale=False)
     return fig
 
-app.run_server(debug=True)
+if __name__ == '__main__':
+    app.run_server(debug=True)
--- orbit.py
+++ orbit.py
@@ -1,10 +1,6 @@
-import matplotlib.pyplot as plt
-import plotly.express as px
-import plotly.graph_objects as go
 import numpy as np
 import datetime as dt
 import math as m
-import pandas as pd
 
 # Earth orbit parameters
 mu = 398600.4418
@@ -31,59 +27,3 @@ def get_orbit(data):
             orbit_data['y'].append(P[1][0])
             orbit_data['z'].append(P[2][0])
     return orbit_data
-
-
-if __name__ == '__main__':
-    tle = []
-    with open('tianhe-tle-short.txt',"r") as f:
-        tle = f.readlines()
-
-    orbit_data = get_orbit(tle)
-    fig = px.line_3d(orbit_data, x="x", y="y", z="z")
-    fig.show()
-    
-
-
-
-
-
-
-
-# def plot_tle(data):
-#     fig = plt.figure()
-#     ax = plt.axes(projection='3d',computed_zorder=False)
-#     for i in range(len(data)//2):
-#         if data[i*2][0] != "1":
-#             print("Wrong TLE format at line "+str(i*2)+". Lines ignored.")
-#             continue
-#         if int(data[i*2][18:20]) > int(dt.date.today().year%100):
-#             orb = {"t":dt.datetime.strptime("19"+data[i*2][18:20]+" "+data[i*2][20:23]+" "+str(int(24*float(data[i*2][23:33])//1))+" "+str(int(((24*float(data[i*2][23:33])%1)*60)//1))+" "+str(int((((24*float(data[i*2][23:33])%1)*60)%1)//1)), "%Y %j %H %M %S")}
-#         else:
-#             orb = {"t":dt.datetime.strptime("20"+data[i*2][18:20]+" "+data[i*2][20:23]+" "+str(int(24*float(data[i*2][23:33])//1))+" "+str(int(((24*float(data[i*2][23:33])%1)*60)//1))+" "+str(int((((24*float(data[i*2][23:33])%1)*60)%1)//1)), "%Y %j %H %M %S")}
-#             orb.update({"name":data[i*2+1][2:7],"e":float("."+data[i*2+1][26:34]),"a":(mu/((2*m.pi*float(data[i*2+1][52:63])/(D*3600))**2))**(1./3),"i":float(data[i*2+1][9:17])*m.pi/180,"RAAN":float(data[i*2+1][17:26])*m.pi/180,"omega":float(data[i*2+1][34:43])*m.pi/180})
-#             orb.update({"b":orb["a"]*m.sqrt(1-orb["e"]**2),"c":orb["a"]*orb["e"]})
-#             R = np.matmul(np.array([[m.cos(orb["RAAN"]),-m.sin(orb["RAAN"]),0],[m.sin(orb["RAAN"]),m.cos(orb["RAAN"]),0],[0,0,1]]),(np.array([[1,0,0],[0,m.cos(orb["i"]),-m.sin(orb["i"])],[0,m.sin(orb["i"]),m.cos(orb["i"])]])))
-#             R = np.matmul(R,np.array([[m.cos(orb["omega"]),-m.sin(orb["omega"]),0],[m.sin(orb["omega"]),m.cos(orb["omega"]),0],[0,0,1]]))
-#             x,y,z = [],[],[]
-#         for i in np.linspace(0,2*m.pi,100):
-#             P = np.matmul(R,np.array([[orb["a"]*m.cos(i)],[orb["b"]*m.sin(i)],[0]]))-np.matmul(R,np.array([[orb["c"]],[0],[0]]))
-#             x += [P[0]]
-#             y += [P[1]]
-#             z += [P[2]]
-#         ax.plot(x,y,z,zorder=5,label=orb["name"])
-#     u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
-#     ax.plot_wireframe(r*np.cos(u)*np.sin(v),r*np.sin(u)*np.sin(v),r*np.cos(v),color="b",alpha=0.5,lw=0.5,zorder=0)
-#     plt.title("Orbits plotted in the ECE frame as of "+orb["t"].strftime("%m %Y"))
-#     ax.set_xlabel("X-axis (km)")
-#     ax.set_ylabel("Y-axis (km)")
-#     ax.set_zlabel("Z-axis (km)")
-#     ax.xaxis.set_tick_params(labelsize=7)
-#     ax.yaxis.set_tick_params(labelsize=7)
-#     ax.zaxis.set_tick_params(labelsize=7)
-#     ax.set_aspect('equal', adjustable='box')
-#     if len(data)//2 < 5:
-#         ax.legend()
-#     else:
-#         fig.subplots_adjust(right=0.8)
-#         ax.legend(loc='center left', bbox_to_anchor=(1.07, 0.5), fontsize=7)
-#     return fig
